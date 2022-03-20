1f0c4da1d47d67ef0256aa20710529e077f934a8
Albert Ye <albert.ye.728@gmail.com>
2022-03-20 00:43:06 -0700

    fixed yellow text functionality

--- shordle.sh
+++ shordle.sh
@@ -6,28 +6,46 @@ RES='\033[0m'
 
 # pick the random word
 KEY=$(sort -R words.txt | awk 'NR <= 1 { print $1 }')
-echo $KEY
+# echo $KEY
 
 echo "we have a cool word. it's guaranteed to be 5 letters long. you have six tries. now guess."
 right=0
-#STR=$(echo ^\($(paste -sd'|' words.txt)\)$)
-for i in {1..6}
-do
-	while true
-	do
+
+for i in {1..6}; do
+	while true; do
 		echo "attempt $i: input a line"
 		read word
-		#if [[ "$word" =~ $(echo ^\($(paste -sd'|' words.txt)\)$) ]]; then
-		#echo $STR
+		# grep finds if a word occurs in the dictionary
 		if grep -Fxq "$word" words.txt; then
 			resu=""
-			for j in {0..4}
-			do
+			for j in {0..4}; do
 				ch1=${word:$j:1}
 				ch2=${KEY:$j:1}
+				nocc=0
+				nbef=0
+				for k in {0..4}; do
+					if [ $k == $j ]; then
+						break
+					fi
+					c=${word:$k:1}
+					if [ $ch1 == $c ]; then
+						nbef=$(($nbef+1))
+					fi
+				done
+				for k in {0..4}; do
+					c1=${word:$j:1}
+					c2=${KEY:$k:1}
+					c3=${word:$k:1}
+					if [ $c1 == $c2 -a $c2 != $c3 ]; then
+						nocc=$(($nocc+1))
+					fi
+				done
+				# echo $nocc
+				# echo $nbef
+				# echo $j
 				if [ $ch1 == $ch2 ]; then
 					export resu=$resu${GRN}$ch1${RES}
-				elif [[ $KEY == *$ch1* ]]; then # this part needs to be somewhat changed
+				elif [[ $nocc -gt $nbef ]]; then # this part needs to be somewhat changed
 					export resu=$resu${YEL}$ch1${RES}
 				else
 					export resu=$resu${RED}$ch1${RES}
@@ -50,6 +68,6 @@ do
 done
 
 if [[ "$right" == "0" ]]; then
-	echo "${RED}ur bad lmao${RES}"
+	echo "${RED}ur bad lol${RES}"
 fi
 echo "the word was \"$KEY\""
